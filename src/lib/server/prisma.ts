468f102fbbc141361e8f0253ea69c6420e6e893f
seshunASU <seshun@asu.edu>
2025-04-17 23:09:18 -0700

    Delete src directory

--- src/lib/server/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma, PrismaClient } from '@prisma/client';
-import type { Confirmed, Deceased, Recovered } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export { prisma };
-export type { Confirmed, Deceased, Recovered }
